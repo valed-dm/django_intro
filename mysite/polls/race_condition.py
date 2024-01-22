f0d1eb91e04b1febfe4a7c56b3ce1f4717c4795f
valed-dm <dmvaled@gmail.com>
2024-01-23 00:01:16 +0300

    race_condition.py added

---
+++ mysite/polls/race_condition.py
@@ -0,0 +1,36 @@
+import threading
+
+from .models import Choice, Question
+
+
+def like():
+    que = Question.objects.first()
+    option = que.choice_set.first()
+    option.votes += 1
+    option.save()
+
+
+def thread_task():
+    for i in range(20):
+        like()
+
+
+def main():
+    que = Question.objects.first()
+    option = que.choice_set.first()
+    option.votes = 0
+    option.save()
+
+    t1 = threading.Thread(target=thread_task)
+    t2 = threading.Thread(target=thread_task)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+
+for i in range(3):
+    main()
+    print(f"Iteration {i} vote counter: {Choice.objects.first().votes}")
