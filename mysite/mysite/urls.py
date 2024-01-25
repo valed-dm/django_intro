b21d1601be651e0565624133f4548695d61f741e
valed-dm <dmvaled@gmail.com>
2024-01-25 18:25:25 +0300

    debug_toolbar adjustment in urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -12,8 +12,10 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,3 +23,11 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+
+# add debug toolbar in urlpatterns
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
