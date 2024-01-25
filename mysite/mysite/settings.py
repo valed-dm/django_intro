be4d138762298f5dacb1ed0cbfed954c26c86917
valed-dm <dmvaled@gmail.com>
2024-01-25 18:23:29 +0300

    debug_toolbar adjustment in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,3 +120,24 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# debug_toolbar moved here.
+if DEBUG:
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+    # this is the main reason for not showing up the toolbar
+    import mimetypes
+
+    mimetypes.add_type("application/javascript", ".js", True)
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "INTERCEPT_REDIRECTS": False,
+    }
