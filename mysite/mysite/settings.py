7a7655ede24b1f5bfa6e67e0a94af82caa61b843
valed-dm <dmvaled@gmail.com>
2024-02-10 00:25:39 +0300

    django-polls is installed as a user library

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    # "polls.apps.PollsConfig",
+    "django_polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    # path("polls/", include("polls.urls")),
+    path("polls/", include("django_polls.urls")),
     path("admin/", admin.site.urls),
 ]
 
